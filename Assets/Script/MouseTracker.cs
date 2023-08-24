402145076424e5e779b7ec74de050f9ddafe9f6b
Maireno <gabrielmaireno@hotmail.com>
2023-08-24 15:48:16 -0300

    posicao da arma

---
+++ Assets/Script/MouseTracker.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseTracker : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePos.x = mousePos.x - 0.27f;
+        mousePos.y = -8.65f;
+        mousePos.z = 10f;
+        transform.position = mousePos;
+
+
+    }
+}
